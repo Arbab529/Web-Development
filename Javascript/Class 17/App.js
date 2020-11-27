6ac114d6894acf1b24279b78482f650f853e1273
Arbab Arain <arbabanjum0347@gmail.com>
2020-11-28 01:11:43 +0500

    Stop Watch

--- Javascript/Class 17/App.js
+++ Javascript/Class 17/App.js
@@ -31,14 +31,44 @@
 // };
 // setTimeout(Clear, 10000);
 
-var count = 0;
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var mininterval = document.getElementById("min");
+var secinterval = document.getElementById("sec");
+var msecinterval = document.getElementById("mili");
+
 var interval;
-function counter() {
-    count++;
-    console.log(count);
+
+function timer() {
+    msec++;
+    msecinterval.innerHTML = msec;
+    if (msec >= 100) {
+        sec++;
+        secinterval.innerHTML = sec;
+        msec = 0;
+        if (sec >= 60) {
+            min++;
+            mininterval.innerHTML = min;
+            sec = 0;
+        }
+    }
 }
-interval = setInterval(counter, 1000);
-function clearcounter() {
+function start() {
+    interval = setInterval(timer, 10);
+}
+
+function stop() {
     clearInterval(interval);
 }
-setTimeout(clearcounter, 11000);
+function reset() {
+    min = 0;
+    sec = 0;
+    msec = 0;
+    mininterval.innerHTML = min;
+    secinterval.innerHTML = sec;
+    msecinterval.innerHTML = msec;
+    clearInterval(interval);
+}
+
