e09169b8deae9f7f7bf075118bfb2047c6d5f752
Arbab Arain <arbabanjum0347@gmail.com>
2020-12-12 19:45:43 +0500

    Destructuring

---
+++ Javascript ES6/6 Destructuring/script.js
@@ -0,0 +1,24 @@
+// const name = "Muhammad Arbab Anjum";
+// let nameArr = name.split(' ');
+
+// let obj1 = {
+//     firstname: nameArr[0],
+//     middlename: nameArr[1],
+//     lastname: nameArr[2]
+// }
+
+// let [name0, name1] = nameArr;
+// console.log(`${name0}`, `${name1}`);
+
+// let [firstname, , lastname] = nameArr;
+// console.log(`${firstname}`, `${lastname}`);
+
+
+let firstname = "arbab";
+let lastname = "arain";
+
+let obj1 = {
+    firstname,
+    lastname
+}
+console.log(obj1);
