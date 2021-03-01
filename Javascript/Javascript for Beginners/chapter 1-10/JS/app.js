c5c1316636012b0800599a52f4f9254a2bea1af8
M ARBAB ANJUM <arbabanjum0347@gmail.com>
2021-03-02 00:12:49 +0500

    JS 1-10

---
+++ Javascript/Javascript for Beginners/chapter 1-10/JS/app.js
@@ -0,0 +1,51 @@
+//////////////////////////////////////////Chapter 1
+
+// alert("Hello How Are You");
+// alert("Error! Please enter a valid password.");
+// alert("Welcome to JS Land... \n Happy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+//////////////////////////////////////////Chapter 2
+
+// var userName = "Arbab Arain";
+// var age = "21 years old";
+// var certification = "Certified Web Developer";
+// alert(userName);
+// alert(age);
+// alert(certification);
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// var email = "example@example.com";
+// alert("My email address is " + email);
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// document.write("Yah! I can write HTML content trough JavaScript");
+
+
+// var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(pattern);
+
+//////////////////////////////////////////Chapter 3
+
+// var age = 15;
+// alert("I am " + age + " years old");
+
+// var visitorName = "Arbab Arain";
+// var productTitle = "T-Shirts(s)";
+// var quantity = 5;
+// alert(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");
+
+
+
+//////////////////////////////////////////Chapter 4
+//////////////////////////////////////////Chapter 5
+//////////////////////////////////////////Chapter 6
+//////////////////////////////////////////Chapter 7
+//////////////////////////////////////////Chapter 8
+//////////////////////////////////////////Chapter 9
+//////////////////////////////////////////Chapter 10
+
